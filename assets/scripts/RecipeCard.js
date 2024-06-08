c4f3073ff74359c60307da3cf03887c7857838cb
nicole-beatrice-go <119998974+nicole-beatrice-go@users.noreply.github.com>
2024-06-07 23:39:18 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -9,9 +9,9 @@ class RecipeCard extends HTMLElement {
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
 		const shadow = this.attachShadow({ mode: 'open' });
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
-		const article = document.createElement('article');
+		const article = document.createElement("article");
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-		const style = document.createElement('style');
+		const style = document.createElement("style");
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
 		style.textContent = `
 		* {
@@ -138,8 +138,8 @@ class RecipeCard extends HTMLElement {
   		</p>
   		<p class="organization">${data.organization}</p>
   		<div class="rating">
-    		<span>${data.ratings}</span>
-    		<img src="/assets/images/icons/5-star.svg" alt="${data.ratings} stars">
+    		<span>${data.rating}</span>
+    		<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
     		<span>(${data.numRatings})</span>
   		</div>
   		<time>${data.lengthTime}</time>
@@ -147,7 +147,7 @@ class RecipeCard extends HTMLElement {
 		  ${data.ingredients} 
   		</p>`;
 
-		this.curr_article.innerHTML = template;
+		curr_article.innerHTML = template;
 	}
 }
 
