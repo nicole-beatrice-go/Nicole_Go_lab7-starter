70506d2d86cdfeed245e07f07761f8329862d8c6
nicole-beatrice-go <119998974+nicole-beatrice-go@users.noreply.github.com>
2024-06-07 20:43:33 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,13 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	const recipe_list = localStorage.getItem('recipes');
+	if (recipe_list) {
+		return JSON.parse(recipe_list);
+	}
+	else {
+		return [];
+	}
 }
 
 /**
@@ -34,11 +41,18 @@ function getRecipesFromStorage() {
  * @param {Array<Object>} recipes An array of recipes
  */
 function addRecipesToDocument(recipes) {
-	// A10. TODO - Get a reference to the <main> element
+		// A10. TODO - Get a reference to the <main> element
+	const main_elem = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	for (let i = 0; i < recipes.length; i++) {
+		const recipeCard = document.createElement('recipe-card');
+		const recipe = recipes[i];
+		recipeCard.data = recipe;
+		main_elem.appendChild(recipeCard);
+	}
 }
 
 /**
